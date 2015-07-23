e5a26d5c250f54c15c4c4a53340ebeca9b2331ef
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2015-07-23 06:50:58 -0500

    update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # from flask.ext.script import Manager
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 # manager = Manager(app)
 
@@ -16,6 +17,7 @@ def ec():
 @app.route('/ec/<name>')
 def ecSession(name):
 	return render_template('englishCorner/ec' + name + '.html')
+
 
 #=====================================================
 
@@ -161,71 +163,41 @@ def consonants(name):
 	return render_template('posts/発音/consonants/' + name + '.html')
 
 
+# @app.route('/ec/vote')
+# def vote():
+# 	return render_template('englishCorner/vote.html')
 
-# @app.context_processor
-# def inject_user():
-#     return {
-# 		"posts": [
-# 			("名詞", "名詞"),
-# 			("動詞", "動詞"), 
-# 			("名詞２ - 冠詞", "名詞２"),
-# 			("形容詞", "形容詞"),
-# 			("動詞２ - 状態を表す動詞", "動詞２"), 
-# 			("副詞", "副詞"),
-# 			("前置詞", "前置詞"),
-# 			("現在形(I do)", "現在形"),
-# 			("現在進行形(I am doing)", "現在進行形"),
-# 			("be動詞(I am~, I was~)", "be動詞"),
-# 			("過去形(I did)", "過去形"),
-# 			("過去進行形(I was doing)", "過去進行形"),
-# 			("不定詞","不定詞"),
-# 			("命令形", "命令形"),
-# 			("接続詞", "接続詞"),
-# 			("名詞３", "名詞３"),
-# 			("現在完了形１(I have done)", "現在完了形１"),
-# 			("現在完了形２(I have done)", "現在完了形２"),
-# 			("現在完了進行形(I have been doing)", "現在完了進行形"),
-# 			("過去完了形(I had done)", "過去完了形"),
-# 			("過去完了進行形(I had been doing)", "過去完了進行形"),
-# 			("used to (do)", "used_to"),
-# 			("(I'm) going to (do)", "going_to"),
-# 			("will", "will"),
-# 			("現在形を使った未来形(I am doing/I do)", "現在形を使った未来形"),
-# 			("can", "can"),
-# 			("When (I do)", "When_I_do"),
-# 			("must", "must"),
-# 			("mustとhave toの違い","must,have_to"),
-# 			("might", "might"),
-# 			("may", "may"),
-# 			("should", "should"),
-# 			("would", "would"),
-# 			("If (I do)", "If_I_do"),
-# 			("could", "could"),
-# 			("受動態 (is done)", "受動態"),
-# 			("There (is)", "there_is"),
-# 			("some, any","some,any"),
-# 			("no, none", "no,none"),
-# 			("数や量を表す代名詞","数や量を表す代名詞"),
-# 			# ("all,every,whole,each",""),
-# 			# ("another,other",""),
-# 			("both, neither, either","both,neither,either"),
-# 			("名詞節","名詞節"),
-# 			("ingとedで終わる形容詞","ingとedで終わる形容詞"),
-# 			("形容詞節（関係代名詞）","形容詞節"),
-# 			("形容詞節２","形容詞節２"),
-# 			("形容詞節３","形容詞節３"),
-# 			("形容詞節４","形容詞節４"),
-# 			("動詞の形容詞的活用","動詞の形容詞的活用"),
-# 			("比較級","比較級"),
-# 			("比較級２","比較級２"),
-# 			("最上級","最上級"),
-# 			("動名詞","動名詞"),
-# 			# ("",""),					
-# 			# ("",""),					
-# 			# ("",""),					
-# 			# ("",""),					
-# 		]
-#     }
+# @app.route('/ec/vote/japanese', methods=['GET', 'POST'])
+# def voteJapanese():
+# 	japanese = ["Saki","Asuka","Dai","Yuri","Nozomi","Yutori","Maki","Kodai","Rira","Ko","Shun","Misato","Miku","Yujiro","Sayaka","Susumu","Kohtaroh","Aoi"]
+# 	checked = []
+# 	error, success = None, None
+
+# 	if request.method == 'POST':
+# 		for i in japanese:
+# 			check = request.form.get(i)
+# 			if check:
+# 				checked.append(i)
+# 		if len(checked) > 3:
+# 			error = "You may not choose more than 3 students!"
+# 		elif len(checked) == 1:
+# 			error = "You must choose at least 1 student!"
+# 		else:
+# 			success = checked
+
+# 	return render_template('englishCorner/voteJapanese.html', japanese=japanese, error=error, success=success)
+
+# @app.route('/ec/vote/korean', methods=['GET', 'POST'])
+# def voteKorean():
+# 	korean = ["Nam Yeon Jae","Yun Gyeong Lee(Celina)","Jisuk Park","Jong Bok Lee","Hyerin Joo(Joo)","Hwan Ju Oh","Min Kyung Kim","Jae Hyung Jin","Da-seul Byun(Trudy)","Seung Hwan Lee","Arim Lee(Tina)","Seowoo Park","Jong Hun Yu","Yu Been Lee","Ju Yeon Kim","Junhyeong Lee","Hyeri Kim(Harry)","Eungyo Lee","Shinae Hwang","Daseul Lee"]
+
+# 	if request.method == 'POST':
+# 		for i in korean:
+# 			check = request.form.get(i)
+# 			print (check)
+
+# 	return render_template('englishCorner/voteKorean.html', korean=korean)
+
 
 if __name__ == '__main__':
 	app.run()
